5d6cc7dec7de46d9616db11abca15af6a8a64cd3
Brandon Davies <brands2k7@gmail.com>
2024-08-18 20:07:34 +0700

    added home page

---
+++ Page_0.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+# Labelling
+st.sidebar.markdown("# Home 🕹")
+st.sidebar.markdown("Feel free to navigate through the different pages, choose your input and let the magic happen!")
+st.title("Home 🕹")
+st.write("Welome to the Swiss Army Knife of summarising media options!")
