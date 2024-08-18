f41fb5e97379231dfbade367ad565a99f88c5783
Brandon Davies <brands2k7@gmail.com>
2024-08-18 22:04:34 +0700

    image resize

--- Page_0.py
+++ Page_0.py
@@ -25,10 +25,10 @@ with col5:
     st.image("https://images.unsplash.com/photo-1517180102446-f3ece451e9d8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
     st.page_link("Page_5.py", label="Text", icon="🔤")
 
+st.write("# Contact me if you have any questions or isses, you can find me here!")
 # Social links
 
 with col1:
-    st.write("Contact me if you have any questions or isses, you can find me here!")
     st.image("https://images.unsplash.com/photo-1611944212129-29977ae1398c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
     st.page_link("https://www.linkedin.com/in/brandon-davies-cs/", label="LinkedIn", icon="1️⃣")
 with col2:
@@ -39,7 +39,7 @@ with col3:
     st.page_link("https://www.linkedin.com/newsletters/7170962682852859904/", label="My Newsletter", icon="3️⃣")
 with col4:
     st.image("https://images.unsplash.com/photo-1560472354-b33ff0c44a43?q=80&w=2151&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
-    st.page_link("https://public.tableau.com/app/profile/brandon.davies/vizzes", label="Tableau", icon="4️⃣")
+    st.page_link("https://public.tableau.com/app/profile/brandon.davies/vizzes", label="Tableau", icon="4️⃣",use_column_width="auto")
 with col5:
     st.image("https://media.licdn.com/dms/image/D560BAQGChCB_hK7nQw/company-logo_200_200/0/1719257926517/elegance_group_nz_logo?e=2147483647&v=beta&t=PIoQsPcECJd4wAHkUugj4NygaUp1J-ooy-9dYJ_AKgM")
     st.page_link("https://elegancegroup.co.nz/", label="Elegance Group", icon="5️⃣")
