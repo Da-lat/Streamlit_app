5d7689e2e8dca22dd0a8fe7bed701495d3773826
Brandon Davies <brands2k7@gmail.com>
2024-08-18 21:57:19 +0700

    column adjustment

--- Page_0.py
+++ Page_0.py
@@ -6,7 +6,7 @@ st.sidebar.markdown("Feel free to navigate through the different pages, choose y
 st.title("Home 🛸")
 st.write("Welome to the Swiss Army Knife of summarising media options!")
 
-col1, col2, col3, col4, col5, col6, col7, col8, col9, col10 = st.columns(10)
+col1, col2, col3, col4, col5, col6, col7, col8, col9, col10 = st.columns(5)
 
 # Navigation links
 with col1:
@@ -28,20 +28,20 @@ with col5:
 st.write("Contact me if you have any questions or isses, you can find me here!")
 # Social links
 
-with col6:
+with col1:
     st.image("https://images.unsplash.com/photo-1611944212129-29977ae1398c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
     st.page_link("https://www.linkedin.com/in/brandon-davies-cs/", label="LinkedIn", icon="1️⃣")
-with col7:
+with col2:
     st.image("https://live.staticflickr.com/5622/22160892602_e5474a698d_w.jpg")
     st.page_link("https://github.com/Da-lat/", label="Github", icon="2️⃣")
-with col8:
+with col3:
     st.image("https://images.unsplash.com/photo-1596526131083-e8c633c948d2?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
     st.page_link("https://www.linkedin.com/newsletters/7170962682852859904/", label="My Newsletter", icon="3️⃣")
-with col9:
+with col4:
     st.image("https://live.staticflickr.com/974/41425693524_d73d3db449_w.jpg")
     st.page_link("https://public.tableau.com/app/profile/brandon.davies/vizzes", label="Tableau", icon="5️⃣")
     st.image("https://media.licdn.com/dms/image/D560BAQGChCB_hK7nQw/company-logo_200_200/0/1719257926517/elegance_group_nz_logo?e=2147483647&v=beta&t=PIoQsPcECJd4wAHkUugj4NygaUp1J-ooy-9dYJ_AKgM")
     st.page_link("https://elegancegroup.co.nz/", label="Elegance Group", icon="4️⃣")
-with col10:
+with col5:
     st.image("https://media.licdn.com/dms/image/D560BAQGChCB_hK7nQw/company-logo_200_200/0/1719257926517/elegance_group_nz_logo?e=2147483647&v=beta&t=PIoQsPcECJd4wAHkUugj4NygaUp1J-ooy-9dYJ_AKgM")
     st.page_link("https://elegancegroup.co.nz/", label="Elegance Group", icon="5️⃣")
