f6d855fda81052c7744832af6dc072a5328cee80
Brandon Davies <brands2k7@gmail.com>
2024-08-18 22:45:45 +0700

    ordering2

--- Page_0.py
+++ Page_0.py
@@ -4,27 +4,27 @@ import streamlit as st
 st.sidebar.markdown("# Swiss Army Summaries 🛸")
 st.sidebar.markdown("Feel free to navigate through the different pages, choose your input and let the magic happen!")
 st.title("Home 🛸")
-st.write("Welome to the Swiss Army Knife of summarising media options!")
+st.write("***")
+st.write("## Welome to the Swiss Army Knife of summarising media options! Select an option for your input below to get started.")
 
 col1, col2, col3, col4, col5 = st.columns(5)
-st.write("***")
 
 # Navigation links
 with col1:
     st.image("https://images.unsplash.com/photo-1611162616475-46b635cb6868?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
-    st.page_link("Page_1.py", label="Youtube Analysis", icon="🎈")
+    st.page_link("Page_1.py", label="Youtube", icon="🎈")
 with col2:
     st.image("https://images.unsplash.com/photo-1553484771-371a605b060b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
-    st.page_link("Page_2.py", label="PDF Analysis", icon="📄")
+    st.page_link("Page_2.py", label="PDF", icon="📄")
 with col3:
     st.image("https://images.unsplash.com/photo-1617854818583-09e7f077a156?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
-    st.page_link("Page_3.py", label="URL Analysis", icon="💻")
+    st.page_link("Page_3.py", label="URL", icon="💻")
 with col4:
     st.image("https://images.unsplash.com/photo-1551288049-bebda4e38f71?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
-    st.page_link("Page_4.py", label="CSV AnalysisAnalysis", icon="📈")
+    st.page_link("Page_4.py", label="CSV", icon="📈")
 with col5:
     st.image("https://images.unsplash.com/photo-1517180102446-f3ece451e9d8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
-    st.page_link("Page_5.py", label="Text Analysis", icon="🔤")
+    st.page_link("Page_5.py", label="Text", icon="🔤")
 
 st.write("***")
 st.write("## Contact me if you have any questions or isses, you can find me here!")
