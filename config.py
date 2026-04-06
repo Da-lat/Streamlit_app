60098269cf320dcb384f1a93036aa5c7e73fc073
Brandon Davies <brands2k7@gmail.com>
2026-04-06 18:09:11 +0100

    Good_practice

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 API_KEY = str(st.secrets["API_KEY"]).strip()
-RIOT_API_KEY = "RGAPI-6510fff2-a0df-4b1d-9a2f-a8dfdb44a975"
+RIOT_API_KEY = str(st.secrets["RIOT_API_KEY"]).strip()
 MODEL = "gemini-2.5-flash"
